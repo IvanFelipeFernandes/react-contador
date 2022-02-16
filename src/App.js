e896cf744041550e1a6ed40704be6c353fad8abf
IvanFelipeFernandes <ivanfelipesenai@gmail.com>
2022-02-16 18:47:42 -0300

    js finalizado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    numero: 0
+  };
+
+  add = () => {
+    this.setState({
+      numero: this.state.numero < 10 ? this.state.numero + 1 : 10
+    });
+  };
+
+  remove = () => {
+    this.setState({
+      numero: this.state.numero > 0 ? this.state.numero - 1 : 0
+    });
+  };
+  clear = () => {
+    this.setState({
+      numero: 0
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Contador</h1>
+        <h2>{this.state.numero}</h2>
+        <button onClick={this.add}>+</button>
+        <button onClick={this.remove}>-</button>
+        <button onClick={this.clear}>C</button>
+      </div>
+    );
+  }
 }
 
 export default App;
